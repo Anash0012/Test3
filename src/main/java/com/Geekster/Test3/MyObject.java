439136041bec3b533b515f2ddc09dd2f5c835b83
Anash khan <khananash2003@gmail.com>
2023-09-10 21:56:30 +0530

    Test3

---
+++ src/main/java/com/Geekster/Test3/MyObject.java
@@ -0,0 +1,26 @@
+package com.Geekster.Test3;
+
+import org.springframework.stereotype.Component;
+
+
+public class MyObject {
+
+    private String name;
+    private int age;
+
+
+    public MyObject(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MyObject{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
